27858c02ef0161b3c3fabf22cd90ec7dde997d35
Anurag <anurag.kumar@feryservices.com>
2020-04-22 21:28:51 +0530

    Advance express topics - config, debug, logging

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const config = require('config');
+const debug = require('debug')('app:normal');
+const debugDB = require('debug')('app:db');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+console.log('App Name: ' + config.get('name'));
+console.log('Mail server: ' + config.get('mail.host'));
+console.log('Mail Password: ' + config.get('mail.password'));
+
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+  debug('Morgan is Logging');
+}
+
+app.get('/', (req, res) => {
+  res.send('Welcome to videoplex');
+});
+
+debugDB('Database debugging message');
+
+const port = process.env.PORT || 7000;
+app.listen(port, () => console.log(`App is running at http://localhost:${port} 🚀`));
